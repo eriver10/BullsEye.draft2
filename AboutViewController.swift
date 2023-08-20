66f959113cd446dfd274e6f216a45867994af1f0
E.F.Rivera <eriver10@live.spcollege.edu>
2023-08-20 16:38:46 -0400

    changed to webkit to message pop window

--- AboutViewController.swift
+++ AboutViewController.swift
@@ -6,11 +6,29 @@
 //
 
 import UIKit
+import WebKit
+
 
 class AboutViewController: UIViewController {
+    
   @IBAction func close() {
     dismiss(animated: true, completion: nil)
   }
+    
+  @IBOutlet var webView: WKWebView!
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      if let url = Bundle.main.url(
+        forResource: "BullsEye", withExtension: "html") {
+        let request = URLRequest(url: url)
+        webView.load(request)
+        }
+        
+     }
+    
+    
+    
 }
     
 
