5724da4b50fc5442d57da7ec4d53f1cadf4ebf09
E.F.Rivera <eriver10@live.spcollege.edu>
2023-08-20 18:19:20 -0400

    Bull's Eye Final 1

--- BullsEye.draft2/ViewController.swift
+++ BullsEye.draft2/ViewController.swift
@@ -161,7 +161,18 @@ class ViewController: UIViewController {
       score = 0
       round = 0
       startNewRound()
+        
+        let transition = CATransition()
+          transition.type = CATransitionType.fade
+          transition.duration = 1
+          transition.timingFunction = CAMediaTimingFunction(
+            name: CAMediaTimingFunctionName.easeOut)
+          view.layer.add(transition, forKey: nil)
+        
     }
+    
+    
+    
     
 }
 
