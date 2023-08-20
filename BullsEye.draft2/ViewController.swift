e5deb49854231cf5af38b21bd84f0148a9a9706e
E.F.Rivera <eriver10@live.spcollege.edu>
2023-08-20 12:31:18 -0400

    Almost done Adding view controller next

--- BullsEye.draft2/ViewController.swift
+++ BullsEye.draft2/ViewController.swift
@@ -38,6 +38,26 @@ class ViewController: UIViewController {
         startNewRound()
         startNewGame()
         
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        let thumbImageHighlighted = UIImage( named: "SliderThumb-Highlighted")!
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        let insets = UIEdgeInsets(
+          top: 0,
+          left: 14,
+          bottom: 0,
+          right: 14)
+        let trackLeftImage = UIImage(named: "SliderTrackLeft")!
+        let trackLeftResizable = trackLeftImage.resizableImage(
+          withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        let trackRightImage = UIImage(named: "SliderTrackRight")!
+        let trackRightResizable = trackRightImage.resizableImage(
+          withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+        
+        
+        
         /*
         //This is set to the return from this rounging fuction with parameter
         currentValue = lroundf(slider.value)
