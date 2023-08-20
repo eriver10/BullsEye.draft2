81e04a222a888a8abae8377a7c4fea80af39f964
E.F.Rivera <eriver10@live.spcollege.edu>
2023-08-20 16:06:51 -0400

    draft 2 complete

---
+++ AboutViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AboutViewController.swift
+//  BullsEye.draft2
+//
+//  Created by Grey on 8/20/23.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+  @IBAction func close() {
+    dismiss(animated: true, completion: nil)
+  }
+}
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
